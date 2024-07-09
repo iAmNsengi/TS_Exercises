58b5c882be87ca4b063d9efd556669440b628a85
Eliezer Nsengi <avaranstudios@gmail.com>
2024-07-09 15:17:12 +0200

    generic classes

---
+++ genericClasses.ts
@@ -0,0 +1,12 @@
+class GenericNumber<T> {
+  zeroValue: T;
+  add: (x: T, y: T) => T;
+
+  public constructor(zeroValue: T, add: (x: T, y: T) => T) {
+    this.zeroValue = zeroValue;
+    this.add = add;
+  }
+}
+
+const myGenericnumber = new GenericNumber<number>(0, (x, y) => x + y);
+console.log(myGenericnumber.add(2, 3));
