5926cf991ce4a1253d8c6c3e1f8573f7bfb5db7d
Eliezer Nsengi <avaranstudios@gmail.com>
2024-07-09 09:39:07 +0200

    aliases

---
+++ aliases.ts
@@ -0,0 +1,13 @@
+// TS allows types to be defined separately from the variables that use them.
+type carSize = string;
+type car = {
+  name: string;
+  color?: string;
+};
+
+let mySize: carSize = "45";
+let myCar: car = {
+  name: "BMW",
+};
+
+myCar.color = "red";
