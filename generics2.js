83417e0fe8a0a01a59b8c141c649c48285e132a8
Eliezer Nsengi <avaranstudios@gmail.com>
2024-07-09 15:09:14 +0200

    file: generics2.js

---
+++ generics2.js
@@ -0,0 +1,5 @@
+function getIndex(array, arrayItem) {
+    return array.indexOf(arrayItem);
+}
+var arr = [55, 99, 77];
+console.log(getIndex(arr, 55));
--- generics2.ts
+++ generics2.ts
@@ -1,5 +1,5 @@
 function getIndex<T>(array: T[], arrayItem: T): number {
-  return array.findIndex((item) => item === arrayItem);
+  return array.indexOf(arrayItem);
 }
 const arr = [55, 99, 77];
 console.log(getIndex(arr, 55));
