97dc731d02b35f53c4c2646721ee2110ac83faf4
Eliezer Nsengi <avaranstudios@gmail.com>
2024-07-10 22:20:39 +0200

    partial-required

--- partialRequired.ts
+++ partialRequired.ts
@@ -2,8 +2,12 @@ import { User } from "./omit";
 
 // partial changes all properties to optional
 
-type UserPartial = Partial<User>;
+type UserPartial = Partial<User> & { name: string };
+
+const user: UserPartial = {
+  name: "nsengi",
+};
 
 // required changes all optional to required
 
-type UserRequired = Required<User>;
+type UserRequired = Required<User> & { city?: string };
