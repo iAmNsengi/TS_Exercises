b3f09ee5f8f30a7f463b433891896ed00b9d4530
Eliezer Nsengi <avaranstudios@gmail.com>
2024-07-10 22:28:07 +0200

    record

--- Record.ts
+++ Record.ts
@@ -3,3 +3,26 @@ import { User } from "./omit";
 const users: Record<string, { name: string }> = {
   "1": { name: "nsengi" },
 };
+
+const data = [
+  {
+    name: "John",
+    age: 30,
+  },
+  {
+    name: "jane",
+    age: 25,
+  },
+];
+
+const result = data.reduce(
+  (acc: Record<string, { name: string; age: number }>, cur, index) => {
+    acc[String(index)] = { ...cur };
+    return acc;
+  },
+  {}
+);
+
+console.log(result);
+
+result["1"];
