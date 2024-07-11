2751be4aed23ac68f0e58775b1b35026edbdf2a2
Eliezer Nsengi <avaranstudios@gmail.com>
2024-07-11 09:21:42 +0200

    required.ts

---
+++ gpt/required.ts
@@ -0,0 +1,13 @@
+import { User } from "../omit";
+
+let newUser: Required<User> = {
+  age: 12,
+  name: "nsengi",
+  address: "kigali",
+  email: "ok",
+  password: "1234",
+  phone: "67676",
+  city: "999",
+};
+
+// makes all properties required
